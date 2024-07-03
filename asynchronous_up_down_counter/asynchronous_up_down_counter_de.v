cf5d0bdbbf09adbea0839e31630eb51be3849873
jagan-codes <jagan15092005@gmail.com>
2024-07-03 23:57:05 +0530

    added

---
+++ asynchronous_up_down_counter/asynchronous_up_down_counter_de.v
@@ -0,0 +1,11 @@
+module asynchronous_updown_counter(input up,clk,reset,output reg [3:0] count);
+  always@(posedge clk or posedge reset)
+    begin
+      if(reset)
+        count<=4'b0000;
+      else if(up)
+        count<=count+1;
+      else
+        count<=count-1;
+    end
+endmodule
---
+++ asynchronous_up_down_counter/asynchronous_up_down_counter_tb.v
@@ -0,0 +1,29 @@
+module asynchronous_updown_counter_tb;
+  reg up,clk,reset;
+  wire [3:0] count;
+  
+  asynchronous_updown_counter dut(up,clk,reset,count);
+  initial begin
+    forever #5 clk=~clk;
+  end
+    
+  initial begin
+    $monitor("time=%0t clk=%b reset=%b up=%b count=%b",$time,clk,reset,up,count);
+    $dumpfile("asynchronous_counter.vcd");
+    $dumpvars(1);
+    
+    clk=0;
+    reset=1;
+    up=1;
+    #3;
+    reset=0;
+    #40;
+    up=0;
+    #30;
+    $finish;
+  end
+endmodule
+    
+    
+    
+             
